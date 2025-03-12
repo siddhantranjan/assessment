2875c58174d8005d8815492399c98f44663858e6
Siddhant <siddhant@ncompass.inc>
2025-03-12 19:20:45 +0530

    validation: email validation added

--- src/utils.ts
+++ src/utils.ts
@@ -8,12 +8,21 @@ export const getUserInput = async(): Promise<UserInput> => {
         output: process.stdout,
     });
 
+    const regexList: any = [/^(?:(?!\.{2,}).)*$/, /^([\p{L}\p{M}\p{N}+\-#$!%&_])([\p{L}\p{M}\p{N}.+\-#$!%&_]){0,99}@([\p{L}\p{M}\p{N}.+\-#$!%&_]{1,253}\.){1,8}[a-zA-Z]{2,63}$/u];
+
     const askQuestion = (query: string): Promise<string> => {
         return new Promise((resolve) => rl.question(query, (answer) => resolve(answer)));
     };
 
     const getCredentials = async (): Promise<{ username: string; password: string }> => {
         const username = await askQuestion('Enter your Amazon India username/email: ');
+        const isEmailValid = regexList.every((regex: any) => regex.test(username));
+
+        if (!isEmailValid) {
+            console.error('Invalid email address. Please try again.');
+            return getCredentials();
+        }
+        
         const password = await askQuestion('Enter your Amazon India password: ');
         return { username, password };
     };
